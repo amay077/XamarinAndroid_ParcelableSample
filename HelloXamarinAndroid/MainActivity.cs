bff476ff15b4e5d1f712106a3ecbe2f9eea91858
Hironobu OKUYAMA <okuokuoku@gmail.com>
2013-02-28 19:36:31 +0900

    1st commit

---
+++ HelloXamarinAndroid/MainActivity.cs
@@ -0,0 +1,36 @@
+using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace HelloXamarinAndroid
+{
+    [Activity (Label = "HelloXamarinAndroid", MainLauncher = true)]
+    public class Activity1 : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            var button = FindViewById<Button>(Resource.Id.myButton);
+			
+            button.Click += (sender, e) => 
+            { 
+                button.Text = string.Format("{0} clicks!", count++); 
+            }
+        }
+    }
+}
+
+
